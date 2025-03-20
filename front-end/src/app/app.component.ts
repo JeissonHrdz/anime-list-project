8b4145e6b44b23b935bd34509126796d3dfe74a6
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-03-20 11:30:28 -0500

    ADD: Interface anime details

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from "./Components/top-bar/top-bar.component";
 import { AnimeSearchComponent } from "./Components/anime-search/anime-search.component";
 import { AnimeService } from './Services/anime.service';
+import { MainContainerComponent } from './Components/main-container/main-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent, AnimeSearchComponent],
+  imports: [RouterOutlet, TopBarComponent, AnimeSearchComponent, MainContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { AnimeSearchComponent } from './Components/anime-search/anime-search.component';
 import { AppComponent } from './app.component';
+import { AnimeDetailsComponent } from './Components/main-container/anime-details/anime-details.component';
 
-export const routes: Routes = [    
-   
+export const routes: Routes = [  
+    //{path: 'anime-details', component: AnimeDetailsComponent}     
 ];
