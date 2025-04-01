ca5db17ff7827f202b8f561c374b4afc715e7289
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-04-01 11:34:17 -0500

    UPDATE: Syles search box

--- front-end/src/app/Components/anime/anime-all-characters/anime-all-characters.component.ts
+++ front-end/src/app/Components/anime/anime-all-characters/anime-all-characters.component.ts
@@ -3,6 +3,7 @@ import { AnimeService } from '../../../Core/Services/anime.service';
 import { Character } from '../../../Core/Model/character.model';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { catchError, of, Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-anime-all-characters',
@@ -12,22 +13,29 @@ import { CommonModule } from '@angular/common';
 })
 export class AnimeAllCharactersComponent {
 
+  private destroy$ = new Subject<void>()
   private animeService = inject(AnimeService)
   private route = inject(ActivatedRoute)
 
-  characters?:Array<Character> = []
-  animeId : number = 0
+  characters?: Array<Character> = []
+  animeId: number = 0
 
 
-  ngOnInit(){
-     this.route.paramMap.subscribe((params) => {
+  ngOnInit() {
+    this.route.paramMap.pipe(
+      switchMap(params => {
         this.animeId = Number(params.get('id'));
-        this.animeService.getAllCharacterByAnime(this.animeId)          
-          .subscribe((data: Array<Character>) => {                
-            this.characters = data; 
-            console.log(this.characters)    
-          });  
-      });
+        return this.animeService.getAllCharacterByAnime(this.animeId).pipe(
+          catchError(error => {
+            console.error('Error loading data:', error);
+            return of([]);
+          })
+        );
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe((data: Array<Character>) => {
+      this.characters = data;
+      console.log(this.characters)
+    })
   }
-
 }
