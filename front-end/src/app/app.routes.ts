b828a8de11afbcc53327825d4981cead41e7a916
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-04-18 21:23:25 -0500

    Update: Changes made to app.routes.ts

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -9,31 +9,43 @@ export const routes: Routes = [
     {path: 'anime/:id', 
         loadComponent: () => import('./Components/anime/anime-details/anime-details.component')
         .then(m => m.AnimeDetailsComponent),
-        data: {
-            renderMode: 'client'
-          }    
+        providers: [
+            {
+              provide: 'renderMode',
+              useValue: 'client'
+            }
+        ]   
     }, 
     {path: 'home', component: HomeComponent },  
     {path: 'anime/:id/characters', 
         loadComponent: () => import('./Components/anime/anime-all-characters/anime-all-characters.component')
         .then(m => m.AnimeAllCharactersComponent),
-        data: {
-            renderMode: 'client'
-          }
+        providers: [
+            {
+              provide: 'renderMode',
+              useValue: 'client'
+            }
+        ] 
     },
     {path: 'character/:id', 
         loadComponent: () => import('./Components/characters/character-details/character-details.component')
         .then(m => m.CharacterDetailsComponent),
-        data: {
-            renderMode: 'client'
-          }
+        providers: [
+            {
+              provide: 'renderMode',
+              useValue: 'client'
+            }
+        ] 
     },
     {path: 'voice-actor/:id', 
         loadComponent: () => import('./Components/actor-voice/actor-voice-details/actor-voice-details.component')
         .then(m => m.ActorVoiceDetailsComponent),
-        data: {
-            renderMode: 'client'
-          }
+        providers: [
+            {
+              provide: 'renderMode',
+              useValue: 'client'
+            }
+        ] 
     },
     {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
