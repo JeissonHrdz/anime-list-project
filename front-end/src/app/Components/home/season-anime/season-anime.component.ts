9016dfc9d1351213195d092f425405e6b48de7c6
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-05-08 16:03:51 -0500

    NOT WORK TOLTIP

--- front-end/src/app/Components/home/season-anime/season-anime.component.ts
+++ front-end/src/app/Components/home/season-anime/season-anime.component.ts
@@ -1,19 +1,22 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Directive, inject, signal } from '@angular/core';
 import { AnimeService } from '../../../Core/Services/anime.service';
 import { Anime } from '../../../Core/Model/anime.model';
 import { Subject, takeUntil } from 'rxjs';
-import {NgIcon, provideIcons } from '@ng-icons/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroChevronLeftSolid, heroChevronRightSolid } from '@ng-icons/heroicons/solid';
-import {autoPlacement, hide, offset, shift} from '@floating-ui/dom';
-import  $ from 'jquery';
+import $ from 'jquery';
+
+
 
 @Component({
   selector: 'app-season-anime',
-  imports: [ NgIcon],
+  imports: [NgIcon],
   providers: [provideIcons({ heroChevronLeftSolid, heroChevronRightSolid })],
   templateUrl: './season-anime.component.html',
   styleUrl: './season-anime.component.css'
 })
+
+
 export class SeasonAnimeComponent {
 
   private destroy$ = new Subject<void>()
@@ -24,23 +27,28 @@ export class SeasonAnimeComponent {
     this.animeService.getAnimesActualSeason().pipe(
       takeUntil(this.destroy$)
     ).subscribe((anime) => {
-      this.seasonAnime.set(anime)    
+      this.seasonAnime.set(anime)
     })
   }
 
   rotateAnimes(direction: string) {
-    const itemwidth = $('.item-slide').width() ?? 0;   
+    const itemwidth = $('.item-slide').width() ?? 0;
     const left = $('#season-anime-slider').scrollLeft() ?? 0
-  
-    if  (direction === 'next'){
-      $('#season-anime-slider').animate({scrollLeft: "+="+(itemwidth+25)}, "fast");
-    } else{
-      $('#season-anime-slider').animate({scrollLeft: "-="+(itemwidth+25)}, "fast");
+
+    if (direction === 'next') {
+      $('#season-anime-slider').animate({ scrollLeft: "+=" + (itemwidth + 25) }, "fast");
+    } else {
+      $('#season-anime-slider').animate({ scrollLeft: "-=" + (itemwidth + 25) }, "fast");
     }
-    console.log(left) 
+    console.log(left)
   }
 
+  showDialogAnimeInfo(id: number) {
   
+  }
+    
+  
+
 
   ngOnDestroy(): void {
     this.destroy$.next()
