540f453eca42e62fba78b54ff0d15b5ecbad424f
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-06-16 15:30:35 -0500

    ADD: Format filter search

--- front-end/src/app/Components/search/search.component.ts
+++ front-end/src/app/Components/search/search.component.ts
@@ -1,113 +1,116 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { GenresService } from '../../Core/Services/genres.service';
 import { Genre } from '../../Core/Model/genres.model';
 import $ from 'jquery';
 import { SearchService } from '../../Core/Services/search.service';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroChevronDown } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [NgIcon],
+  providers: [provideIcons({ heroChevronDown })],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
 
 
-private genresService = inject(GenresService);
-private searchService = inject(SearchService)
+  private genresService = inject(GenresService);
+  private searchService = inject(SearchService)
 
-genres?: Genre;
-isVisible = false;
-isVisibleBoxYears = false;
-isVisibleBoxSeason = false;
-countSelectedsGenres = 0;
-nameSelecteFirstGenre: string = '';
-genresSelected: string[] = [];
-yearSelected: number = 0;
-seasonSelected: string = '';
-years: number[] = [];
+  genres?: Genre;
+  isVisibleBoxGenres = false;
+  isVisibleBoxYears = false;
+  isVisibleBoxSeason = false;
+  isVisibleBoxFormat = false;
+
+  countSelectedsGenres = 0;
+  nameSelecteFirstGenre: string = '';
+  genresSelected: string[] = [];
+  yearSelected: number = 0;
+  seasonSelected: string = '';
+  formatSelected: string = '';
+  years: number[] = [];
+  formats = ['TV_Show', 'TV_Short', 'OVA', 'ONA', 'Special', 'Movie', 'Music'];
 
 
   ngOnInit() {
     this.genresService.getGenres().subscribe({
-      next: (data) => {        
-          this.genres = data;
-          console.log('Genres loaded successfully:', this.genres);    
-         
+      next: (data) => {
+        this.genres = data;
+        console.log('Genres loaded successfully:', this.genres);
+
       },
       error: (err) => {
         console.error('Error loading genres:', err);
         console.error(err);
       }
     });
-    
-   this.years = this.searchService.getAllYears();
-  }
-  
-  showBoxGenres(){
-    if(this.isVisible) {
-      $("#genresBox").fadeOut(150);
-      this.isVisible = false;
-      return;
-    }
-    this.isVisible = true;
-    $("#genresBox").fadeIn(150);
+
+    this.years = this.searchService.getAllYears();
   }
 
+
+
   selectedGenresItems(name: string) {
-    if(this.countSelectedsGenres === 0) {
-         this.nameSelecteFirstGenre = name;   
+    if (this.countSelectedsGenres === 0) {
+      this.nameSelecteFirstGenre = name;
     }
-    if(this.genresSelected.includes(name)) {
+    if (this.genresSelected.includes(name)) {
       this.genresSelected = this.genresSelected.filter(item => item !== name);
-      if(this.countSelectedsGenres === 1) {
+      if (this.countSelectedsGenres === 1) {
         this.nameSelecteFirstGenre = '';
       }
-      this.countSelectedsGenres--;     
+      this.countSelectedsGenres--;
       return;
     }
-    this.genresSelected.push(name); 
+    this.genresSelected.push(name);
     this.countSelectedsGenres++;
   }
 
 
-  showBoxYears(){
-    if(this.isVisibleBoxYears) {
-      $("#yearsBox").fadeOut(150);
-      this.isVisibleBoxYears = false;
-      return;
-    }
-    this.isVisibleBoxYears = true;
-    $("#yearsBox").fadeIn(150);
+  toggleBoxVisibility(id: string) {
+    if (id === 'Genres') {    
+     
+        this.isVisibleBoxYears = false;
+        this.isVisibleBoxSeason = false;
+        this.isVisibleBoxFormat = false;
+      } 
+      
+
+    (this as any)[`isVisibleBox${id}`] = !(this as any)[`isVisibleBox${id}`];
+    $(`#${id}Box`)[(this as any)[`isVisibleBox${id}`] ? 'fadeIn' : 'fadeOut'](150);
   }
 
-  getYearSelected(year: number){  
-    if(year != this.yearSelected){
-       $("#"+this.yearSelected).removeClass('selected') 
+  getYearSelected(year: number) {
+    if (year !== this.yearSelected) {
+      $("#" + this.yearSelected).removeClass('selected')
     }
-    $("#"+year).addClass('selected') 
-    this.yearSelected = year;
+    $("#" + year).addClass('selected')
+    this.yearSelected = year;  }
+
+  getSeasonSelected(season: string) {
+    if (season !== this.seasonSelected) {
+      $(this.seasonSelected).removeClass('selected')
+    }
+    $(season).addClass('selected')
+    this.seasonSelected = season  }
+  getFormatSelected(format: string) {
+    if (format !== this.formatSelected) {
+      $(this.formatSelected).removeClass('selected')
+    }
+    $(format).addClass('selected')
+    this.formatSelected = format;
   }
 
-  showBoxSeason(){
-    if(this.isVisibleBoxSeason) {
-      $("#seasonBox").fadeOut(150);
-      this.isVisibleBoxSeason = false;
-      return;
-    }
-    this.isVisibleBoxSeason = true;
-    $("#seasonBox").fadeIn(150);
+
+
+  @HostListener('document:click')
+  onDocumentClick(): void {  
+
   }
 
-  getSeasonSelected(season: string){  
-    
-    if(season !== this.seasonSelected){
-       $("#"+this.seasonSelected).removeClass('selected') 
-    }
-    $("#"+season).addClass('selected') 
-    this.seasonSelected = season;
-  }
-  
 }
 
 
